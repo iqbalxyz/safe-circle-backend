a49758802a1bfa0a11258cc1c619155b4aac6355
moch.iqbal.sudrajat <moch.iqbal.sudrajat@haud.com>
2026-04-26 15:23:32 +0700

    feat: implement express-rate-limit middleware for API request throttling

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import 'dotenv/config';
 import express, { type Application } from 'express';
 import appMiddleware from './middleware/cors.middleware';
+import limiter from './middleware/rate-limiter.middleware';
 import './utils/bigint.util';
 
 const app: Application = express();
 const port: number = process.env.PORT != null ? parseInt(process.env.PORT) : 4000;
 
 app.use(appMiddleware);
+
+app.use(limiter);
 
 app.listen(port, () => {
   console.log(`App listen on port ${port}`);
